f395855733bca917059d0223d795d3fced6160df
Reginaldo <reginaldobonaldi22@gmail.com>
2019-11-19 07:17:01 -0300

    primeiro commit e alguns componentes

---
+++ resources/js/components/Painel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div class="card-header">{{title}}</div>
+
+    <div class="card-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
---
+++ resources/js/components/Topo.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" v-bind:href="url">{{ title}}</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        v-bind:aria-label="toggle_navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto"></ul>
+        <slot></slot>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["title", "url", "toggle_navigation"]
+};
+</script>
+
+<style>
+</style>
